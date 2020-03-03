1b4956f0351c587469efa6dee359e7d9aee1ae3e
srajan <jaimsrajan@gmail.com>
2020-03-04 02:22:00 +0530

    make it better

--- Desktop/hero/app.py
+++ Desktop/hero/app.py
@@ -29,7 +29,7 @@ def analyze():
 def pdf():
 	if request.method == 'POST':
 				render = render_template('pdf.html',para=dic['para'],title=dic['title'],lists=dic['lists'],key=dic['key'])
- 				 config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+ 				config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
 				pdf = pdfkit.from_string(render,False,configuration=config)
 
 				response = make_response(pdf)
