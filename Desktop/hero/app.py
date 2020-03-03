e3495f321668f39575f40dadc201b5c40f1bf5d3
srajan <jaimsrajan@gmail.com>
2020-03-03 19:54:55 +0530

    make it better

--- Desktop/hero/app.py
+++ Desktop/hero/app.py
@@ -1,18 +1,22 @@
-from flask import Flask,render_template,make_response
-import pdfkit
+from flask import Flask,render_template,request
+from temp_nltk import url_rize
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 @app.route('/')
 def index():
-	render = render_template('index1.html')
-	pdf = pdfkit.from_string(render,False)
+	mess = 'I LOVE PYTHON!'
+	return render_template('index1.html',msg=mess)
 
-	response = make_response(pdf)
-	response.headers['Content-Type'] = 'application/pdf'
-	response.headers['Content-Disposition'] = 'attachment; filename=out.pdf'
-
-	return response
+@app.route('/success',methods=['GET','POST'])
+def analyze():
+	if request.method == 'POST':
+		dec = ' (Summarized Form)'
+		key = 'Key Points'
+		rawtext = request.form['raw']
+		typesum = request.form['typesum']
+		protext = url_rize(rawtext,typesum)
+	return render_template('index2.html',rawe=protext[0],title=protext[1],lists=protext[2],dec=dec,key=key)
 
 if __name__ == '__main__':
 	app.run(debug=True)
