5011779955844321fae911531d17527ace7a81d6
srajan <jaimsrajan@gmail.com>
2020-03-03 17:20:19 +0530

    make it better

--- Desktop/hero/app.py
+++ Desktop/hero/app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template,request
+from temp_nltk import url_rize
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'hell0 world'
+	mess = 'I LOVE PYTHON!'
+	return render_template('index1.html',msg=mess)
 
+@app.route('/success',methods=['GET','POST'])
+def analyze():
+	if request.method == 'POST':
+		dec = ' (Summarized Form)'
+		key = 'Key Points'
+		rawtext = request.form['raw']
+		typesum = request.form['typesum']
+		protext = url_rize(rawtext,typesum)
+	return render_template('index2.html',rawe=protext[0],title=protext[1],lists=protext[2],dec=dec,key=key)
+
+if __name__ == '__main__':
+	app.run()
---
+++ Desktop/hero/temp_nltk.py
@@ -0,0 +1,82 @@
+import bs4 as bs
+import urllib.request
+import re
+import nltk
+import heapq
+import validators
+
+
+def url_rize(raw_urls,types):
+    if types == 'ss':
+        thres   = 25
+        sen_num = 5
+        key_sen = 2
+    elif types == 'ls':
+        thres   = 29
+        sen_num = 7
+        key_sen = 4
+    else:
+        thres   = 32
+        sen_num = 15
+        key_sen = 7
+    valid = validators.url(raw_urls)
+    if valid == True:
+        url = raw_urls
+       
+        source = urllib.request.urlopen(url).read()
+        soup = bs.BeautifulSoup(source,'lxml')
+        text = ""
+        title = soup.find('title').text
+        for paragraph in soup.find_all('p'):
+            text += paragraph.text
+    else:
+        text = raw_urls
+       
+    text = re.sub(r'\[[0-9]+\]',' ',text)
+    text = re.sub(r'\s+',' ',text)
+    clean_text = text.lower()
+    clean_text = re.sub(r'\W',' ',clean_text)
+    clean_text = re.sub(r'\d',' ',clean_text)
+    clean_text = re.sub(r'\s+',' ',clean_text)
+
+    sentences = nltk.sent_tokenize(text)
+    stop_words = nltk.corpus.stopwords.words('english')
+
+    word2count = {}
+    for word in nltk.word_tokenize(clean_text):
+        if word not in stop_words:
+            if word not in word2count.keys():
+                word2count[word]=1
+            else:
+                word2count[word] += 1
+
+    for key in word2count.keys():
+        word2count[key] = word2count[key]/max(word2count.values()) 
+        
+    sent2score = {} 
+
+    for sentence in sentences:
+        for word in nltk.word_tokenize(sentence.lower()): 
+            if word in word2count.keys():
+                if len(sentence.split(' ')) < thres:
+                    if sentence not in sent2score.keys():
+                        sent2score[sentence] = word2count[word]
+                    else:
+                        word2count[word] += word2count[word]                 
+    best = heapq.nlargest(sen_num,sent2score,key=sent2score.get)
+    sents2score = {}
+    for sentence in sentences:
+        for word in nltk.word_tokenize(sentence.lower()): 
+            if word in word2count.keys():
+                if len(sentence.split(' ')) < 20:
+                    if sentence not in sents2score.keys():
+                        sents2score[sentence] = word2count[word]
+                    else:
+                        word2count[word] += word2count[word] 
+    key_points = heapq.nlargest(key_sen,sents2score,key=sents2score.get)
+    x = " ".join(best)
+    if valid == True:
+        return [x,title,key_points]
+    else:
+        return[x,'Summary',key_points]
+
