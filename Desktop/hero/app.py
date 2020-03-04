4a44e04820369c16d0328f4a774be757418502cf
srajan <jaimsrajan@gmail.com>
2020-03-05 02:16:53 +0530

    make it better

--- Desktop/hero/app.py
+++ Desktop/hero/app.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template,request,make_response
 from temp_nltk import url_rize
 import pdfkit
-import re
 
 app = Flask(__name__)
 
@@ -10,39 +9,33 @@ app = Flask(__name__)
 def index():
 	return render_template('index1.html')
 
-dic=""
+dic={}
 @app.route('/success',methods=['GET','POST'])
 def analyze():
 	if request.method == 'POST':
-		global dic
 		key = 'Key Points'
 		rawtext = request.form['raw']
 		typesum = request.form['typesum']
 		protext = url_rize(rawtext,typesum)
-		dic = protext[0]
+		dic['para']  = protext[0]
+		dic['title'] = protext[1]
+		dic['lists'] = protext[2]
+		dic['key'] 	 = key
 	return render_template('index2.html',rawe=protext[0],title=protext[1],lists=protext[2],key=key)
 
-@app.route('/pd/<title>/<lists>/<key>',methods=['GET','POST'])
-def pdf(title,lists,key):
+@app.route('/pdf',methods=['GET','POST'])
+def pdf():
 	if request.method == 'POST':
-		dic2 = dic
-		pre = re.sub(r'\'s','!s',lists)
-		pre = re.sub(r'[\'\[\]\"]','`',pre)
-		pre = re.sub(r'`,\s`','~',pre)
-		pre = re.sub(r'!','\'',pre)
-		pre = re.sub(r'``','',pre)
-		pre = re.sub(r'`','"',pre)
-		final = pre.split('~')
 		config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
-		render = render_template('pdf.html',para=dic2,title=title,lists=final,key=key)
+		render = render_template('pdf.html',para=dic['para'],title=dic['title'],lists=dic['lists'],key=dic['key'])
 		pdf = pdfkit.from_string(render, False, configuration=config)
 
 		response = make_response(pdf)
 		response.headers['Content-Type'] = 'application/pdf'
 		response.headers['Content-Disposition'] = 'attachment; filename=summary.pdf'
-	
+
 	return response
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
